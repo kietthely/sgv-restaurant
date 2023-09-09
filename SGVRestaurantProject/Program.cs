fe812790bc12cb9a0ff2490f14403cc169e8f7d5
chacy263 <chacy263@mymail.unisa.edu.au>
2023-09-09 13:50:52 +0930

    login page done

--- SGVRestaurantProject/Program.cs
+++ SGVRestaurantProject/Program.cs
@@ -9,6 +9,13 @@ builder.Services.AddDbContext<SVGRestaurantContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +25,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
