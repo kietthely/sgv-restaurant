90e8747e80728e39040ccb48679deb33f80fbe63
ngumt019 <ngumt019@mymail.unisa.edu.au>
2023-09-09 22:46:25 +0930

    Modified asp actions on delete button to navigate to correct controller action along with banquetId and itemId

--- SGVRestaurantProject/Controllers/BanquetMenusController.cs
+++ SGVRestaurantProject/Controllers/BanquetMenusController.cs
@@ -66,7 +66,7 @@ namespace SGVRestaurantProject.Controllers
         public async Task<IActionResult> DeleteBanquetMenuItem(int? banquetId, int? itemId)
         {
             // query to include the banquet with matching banquetId.
-            var banquetMenu = _context.BanquetMenus
+            var banquetMenu =  _context.BanquetMenus
                 .Include(b => b.BanquetAndMenuItems)
                 .FirstOrDefault(b => b.BanquetId == banquetId);
 
