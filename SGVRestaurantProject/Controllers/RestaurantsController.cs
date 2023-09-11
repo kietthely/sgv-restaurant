8e2fb7b913681caacbe12a1467ffd7577ad18249
ngumt019 <ngumt019@mymail.unisa.edu.au>
2023-09-11 16:50:38 +0930

    test created new user.

--- SGVRestaurantProject/Controllers/RestaurantsController.cs
+++ SGVRestaurantProject/Controllers/RestaurantsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 //using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 //using AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
--- SGVRestaurantProject/Migrations/20230911065515_customUserDataAdded.Designer.cs
+++ SGVRestaurantProject/Migrations/20230911065515_customUserDataAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SGVRestaurantProject.Models;
 
@@ -11,9 +12,10 @@ using SGVRestaurantProject.Models;
 namespace SGVRestaurantProject.Migrations
 {
     [DbContext(typeof(SVGRestaurantContext))]
-    partial class SVGRestaurantContextModelSnapshot : ModelSnapshot
+    [Migration("20230911065515_customUserDataAdded")]
+    partial class customUserDataAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
