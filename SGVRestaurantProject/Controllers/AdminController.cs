94961c590faae65e7bf172751d968334092dbc5f
ngumt019 <ngumt019@mymail.unisa.edu.au>
2023-09-11 16:58:59 +0930

    Created adminController to create  roles

---
+++ SGVRestaurantProject/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SGVRestaurantProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        [HttpGet]
+        public IActionResult ListAllRoles()
+        {
+            var roles = _roleManager.Roles;
+
+            return View(roles);
+        }
+    }
+}
