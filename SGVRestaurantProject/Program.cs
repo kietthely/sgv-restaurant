d37ae86cf00c31cb315857fa09202f27f1d2b579
ngumt019 <ngumt019@mymail.unisa.edu.au>
2023-09-11 19:16:26 +0930

    Added seeding to automatically create admin account on first initialization on new environment.

--- SGVRestaurantProject/Program.cs
+++ SGVRestaurantProject/Program.cs
@@ -48,4 +48,41 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var roles = new[] { "Admin", "Staff", "Customer" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+    }
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<DefaultUser>>();
+
+    var roles = new[] { "Admin", "Staff", "Customer" };
+
+    string email = "admin@sgv.com";
+    string password = "Admin1234!";
+
+    if(await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new DefaultUser();
+        user.UserName = email;
+        user.Email = email;
+        user.FirstName = "Admin";
+        user.LastName = "AdminAdmin";
+        user.EmailConfirmed = true;
+
+        await userManager.CreateAsync(user, password);
+
+        await userManager.AddToRoleAsync(user, "Admin");
+    }
+}
+
 app.Run();
