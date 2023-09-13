697bedcbcb3c9a303d8e032e0a4ce0d70d6e3711
Sammie <lythekiet333@gmail.com>
2023-09-13 15:16:02 +0930

    Update the design of booking controller
    Update the design of booking controller

--- SGVRestaurantProject/Models/Sitting.cs
+++ SGVRestaurantProject/Models/Sitting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace SGVRestaurantProject.Models
 {
@@ -10,12 +11,18 @@ namespace SGVRestaurantProject.Models
             Bookings = new HashSet<Booking>();
             RestaurantSittings = new HashSet<RestaurantSitting>();
         }
-
+        
         public int SittingId { get; set; }
         public string SittingType { get; set; } = null!;
         public int SittingStart { get; set; }
         public int SittingEnd { get; set; }
-
+        public string SittingDuration
+        {
+            get
+            {
+                return SittingStart + " - " + SittingEnd;
+            }
+        }
         public virtual ICollection<Booking> Bookings { get; set; }
         public virtual ICollection<RestaurantSitting> RestaurantSittings { get; set; }
     }
