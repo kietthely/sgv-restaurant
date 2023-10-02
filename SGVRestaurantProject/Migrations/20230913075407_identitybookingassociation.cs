a83bd2deb5fd2b6f386a501dd2a4f8128a610e83
chacy263 <chacy263@mymail.unisa.edu.au>
2023-10-02 16:58:24 +1030

    Completed scrum 26
    Customers can see the contact details of a restaurant now

---
+++ SGVRestaurantProject/Migrations/20230913075407_identitybookingassociation.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGVRestaurantProject.Migrations
+{
+    public partial class identitybookingassociation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
