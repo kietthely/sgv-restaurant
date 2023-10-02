526ce91bfeb239e4a3c518f9070c3b682611ccf4
ngumt019 <ngumt019@mymail.unisa.edu.au>
2023-10-02 14:30:14 +1030

    Create RestaurantBookingsVM.cs

---
+++ SGVRestaurantProject/Models/ViewModel/RestaurantBookingsVM.cs
@@ -0,0 +1,8 @@
+﻿namespace SGVRestaurantProject.Models.ViewModel
+{
+    public class RestaurantBookingsVM
+    {
+        public Restaurant theRestaurant { get; set; }
+        public List<Booking> restaurantBookings { get; set; }
+    }
+}
