087abe6f375de4abee2d9bd38a521025fd95de3a
ngumt019 <ngumt019@mymail.unisa.edu.au>
2023-10-03 13:37:46 +1030

    Added authorization for role admin for admincontroller

--- SGVRestaurantProject/Controllers/AdminController.cs
+++ SGVRestaurantProject/Controllers/AdminController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SGVRestaurantProject.ViewModels;
 
 namespace SGVRestaurantProject.Controllers
 {
+    [Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
