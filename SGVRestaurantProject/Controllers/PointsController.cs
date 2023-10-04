5fe0b4f7b72265ba59848392cc28d31264d9c78f
Ben Koop <koobc002@mymail.unisa.edu.au>
2023-10-04 14:37:40 +1030

    Modified points controller

--- SGVRestaurantProject/Controllers/PointsController.cs
+++ SGVRestaurantProject/Controllers/PointsController.cs
@@ -1,19 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SGVRestaurantProject.Models;
+using SGVRestaurantProject.Models.Users;
 
 namespace SGVRestaurantProject.Controllers
 {
     public class PointsController : Controller
     {
         private readonly SVGRestaurantContext _context;
+        private readonly UserManager<DefaultUser> _userManager;
 
-        public PointsController(SVGRestaurantContext context)
+        public PointsController(UserManager<DefaultUser> userManager, SVGRestaurantContext context)
         {
             _context = context;
+            _userManager = userManager;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Points()
         {
-            return View();
+            // select all bookings that are completed = true.
+            // need to group these by user ID
+            var userPoints = await _context.Bookings
+                .Where(b => b.Completed == "Yes")
+                .GroupBy(b => b.DefaultUserId)
+                .Select(data => new UserPointsViewModel
+                {
+                    userId = data.Key,
+                    NumberOfPoints = data.Count(),
+                    FirstName = data.First().User.FirstName,
+                    LastName = data.First().User.LastName,
+                })
+                .ToListAsync();
+            
+            return View(userPoints);
         }
     }
 }
