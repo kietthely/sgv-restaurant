f868634eb45ba08a5ac6fa6cd7945c564fe252b7
chacy263 <chacy263@mymail.unisa.edu.au>
2023-10-04 14:42:28 +1030

    finished scrum 78

---
+++ SGVRestaurantProject/Controllers/TiersAPIController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SGVRestaurantProject.Models;
+using SGVRestaurantProject.Models.Users;
+using SGVRestaurantProject.Models.ViewModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SGVRestaurantProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiersAPIController : ControllerBase
+    {
+        private readonly SVGRestaurantContext _context;
+        private readonly UserManager<DefaultUser> _userManager;
+
+        public TiersAPIController(UserManager<DefaultUser> userManager, SVGRestaurantContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        // GET: api/<TiersAPIController>
+        [HttpGet]
+        public async Task<string> GetTier(string userName)
+        {
+            var currentUser = await _userManager.FindByNameAsync(userName);
+            BookingPoints bookingPoints = new BookingPoints();
+            var userId = currentUser.Id;
+            #region BookingDetailsQuery
+
+            var bookingDetails = _context.Bookings
+                .Include(r => r.Restaurant)
+                .Include(s => s.Sitting)
+                .Where(u => u.DefaultUserId == userId)
+                .ToList();
+            bookingPoints.BookingDetails = bookingDetails;
+            #endregion
+
+            // points logic
+            var completedBooking = bookingDetails.Where(c => c.Completed == "Yes");
+            bookingPoints.Points = completedBooking.Count();
+            bookingPoints.NumberOfBookings = bookingDetails.Count();
+
+            if (bookingPoints.Points >= 1 && bookingPoints.Points <= 2)
+            {
+                return "Silver";
+            }
+            else if (bookingPoints.Points > 2)
+            {
+                return "Gold";
+            }
+            return "Bronze";
+        }
+    }
+}
--- SGVRestaurantProject/Migrations/20231004032330_scrum78.Designer.cs
+++ SGVRestaurantProject/Migrations/20231004032330_scrum78.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SGVRestaurantProject.Models;
 
@@ -11,9 +12,10 @@ using SGVRestaurantProject.Models;
 namespace SGVRestaurantProject.Migrations
 {
     [DbContext(typeof(SVGRestaurantContext))]
-    partial class SVGRestaurantContextModelSnapshot : ModelSnapshot
+    [Migration("20231004032330_scrum78")]
+    partial class scrum78
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
