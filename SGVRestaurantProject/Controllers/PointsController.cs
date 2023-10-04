82b7fa6610567c1992fd3ebf21d7434276adf502
Ben Koop <koobc002@mymail.unisa.edu.au>
2023-10-04 13:54:54 +1030

    Created controller for points view

---
+++ SGVRestaurantProject/Controllers/PointsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGVRestaurantProject.Models;
+
+namespace SGVRestaurantProject.Controllers
+{
+    public class PointsController : Controller
+    {
+        private readonly SVGRestaurantContext _context;
+
+        public PointsController(SVGRestaurantContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
