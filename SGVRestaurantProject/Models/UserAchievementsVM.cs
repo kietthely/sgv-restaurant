0931aa7ce7f8e2f84ba0c7d31662fc464f65d383
ngumt019 <ngumt019@mymail.unisa.edu.au>
2023-10-04 13:15:09 +1030

    Created a custom viewModel to be used with index of userAchievements

---
+++ SGVRestaurantProject/Models/UserAchievementsVM.cs
@@ -0,0 +1,8 @@
+﻿namespace SGVRestaurantProject.Models
+{
+    public class UserAchievementsVM
+    {
+        public List<Achievements> AllAchievements { get; set; }
+        public List<UserAchievements> UserAchievements { get; set; }
+    }
+}
