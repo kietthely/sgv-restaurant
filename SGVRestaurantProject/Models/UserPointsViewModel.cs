27f9863c6b927d39658469cfe6002d5b64f49c3b
Ben Koop <koobc002@mymail.unisa.edu.au>
2023-10-04 14:37:52 +1030

    Modified UserPointsViewModel

---
+++ SGVRestaurantProject/Models/UserPointsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SGVRestaurantProject.Models.Users;
+
+namespace SGVRestaurantProject.Models
+{
+    public class UserPointsViewModel
+    {
+        public string userId { get; set; }
+        public int NumberOfPoints { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
